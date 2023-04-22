8ead2292500d86304f6e622949f6d428ddc254b8
erhan <erhanpaker@gmail.com>
2023-04-22 17:05:15 +0300

    Qualitydemy

--- src/test/java/pages/QualitydemyPage.java
+++ src/test/java/pages/QualitydemyPage.java
@@ -15,6 +15,23 @@ public class QualitydemyPage {
   @FindBy(xpath = "//*[text()='Accept']")
   public WebElement cookies;
 
+  @FindBy(xpath = "//a[@class='btn btn-sign-in-simple']")
+  public WebElement loginLinki;
+
+  @FindBy(xpath = "//input[@type='email']")
+  public WebElement emailKutusu;
+
+  @FindBy(xpath = "//input[@type='password']")
+  public WebElement passwordKutusu;
+
+  @FindBy(xpath = "(//button[@type='submit'])[2]")
+  public WebElement loginButonu;
+
+  @FindBy(xpath = "//span[@class='fw-500']")
+  public WebElement categories;
+
+  @FindBy(xpath = "//i[@class='fa fa-align-justify']")
+  public WebElement allCourses;
 
 
 
--- src/test/java/tests/test101.java
+++ src/test/java/tests/test101.java
@@ -10,10 +10,17 @@ public class test101 {
     QualitydemyPage qualitydemyPage = new QualitydemyPage();
     @Test
 
+    // proje kodlarım ve anasayafa içerikleri
+
     public void test01(){
         Driver.getDriver().get(ConfigReader.getProperty("url"));
-
         qualitydemyPage.cookies.click();
+        qualitydemyPage.loginLinki.click();
+        qualitydemyPage.emailKutusu.sendKeys(ConfigReader.getProperty("GecerliMail"));
+        qualitydemyPage.passwordKutusu.sendKeys(ConfigReader.getProperty("GecerliPassword"));
+        qualitydemyPage.loginButonu.click();
+        qualitydemyPage.categories.click();
+        qualitydemyPage.allCourses.click();
 
     }
 }
